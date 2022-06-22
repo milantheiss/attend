f25e6192a63980612c9ca0a26a77f3eb9c3e346b
Milan Theiß <milan.theiss@gmx.net>
2022-06-22 15:27:43 +0200

    Client from Backup

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-//import "./registerServiceWorker";
-import router from './router'
-
-import './assets/tailwind.css'
-
-createApp(App).use(router).mount("#app");
--- client/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-const path = require('path')
-
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  outputDir: path.resolve(__dirname, '../server/public')
-}
