d573c4344178c2943be86eb72615a3857c901097
Milan Theiß <milan.theiss@gmx.net>
2022-07-06 22:45:45 +0200

    Imported HttpStatus

--- server/controllers/member.controller.js
+++ server/controllers/member.controller.js
@@ -1,6 +1,7 @@
 const logger = require('../config/logger');
 const {memberService} = require('../services')
 const catchAsync = require('../utils/catchAsync');
+const httpStatus = require('http-status');
 
 const addMember = catchAsync(async (req, res) => {
     const result = await memberService.addMember(req.user, req.body);
