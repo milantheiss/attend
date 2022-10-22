4c0cdc3ad05f5e3d79d5cc20cd3eddc1695a80f4
Milan Theiß <milan.theiss@gmx.net>
2022-10-22 11:50:48 +0200

    Styling bei X verändert

--- client/src/components/MemberEditor.vue
+++ client/src/components/MemberEditor.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white px-3 py-1.5 mt-3 mb-3 rounded-lg drop-shadow-md">
-        <div class="grid place-items-end mt-1" v-if="!createsNewMember">
+        <div class="grid place-items-end mt-1 mr-1" v-if="!createsNewMember">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-8 text-black" @click="onClickOnClose()">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
