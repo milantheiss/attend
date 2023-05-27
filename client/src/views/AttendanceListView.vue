b8f8c85e290c8951f2f5bb2d784789cda94cbdfb
Milan Theiß <milan.theiss@gmx.net>
2023-05-27 15:51:44 +0200

    Stroke Width von X Icon verändert

--- client/src/views/AttendanceListView.vue
+++ client/src/views/AttendanceListView.vue
@@ -19,7 +19,7 @@
           </svg>
 
           <!--Close Icon-->
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor"
             class="w-8 h-8" v-show="showTimesBox">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
